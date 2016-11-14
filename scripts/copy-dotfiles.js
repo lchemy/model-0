92d16c3eadea74880e3c1bb311d003ecc192e20d
jessezhang91 <jessezhang91@gmail.com>
2016-11-14 17:38:50 -0500

    copy dotfiles script

---
+++ scripts/copy-dotfiles.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const fs = require("fs"),
+	path = require("path");
+
+copyFile(".gitignore");
+copyFile(".npmrc");
+
+function copyFile(source, target) {
+	if (target == null) {
+		target = source;
+	}
+	source = path.resolve(path.join(__dirname, "..", source));
+	target = path.resolve(path.join(__dirname, "../dist", target));
+
+	try {
+		fs.accessSync(source);
+		fs.writeFileSync(target, fs.readFileSync(source));
+	} catch(e) {}
+}
