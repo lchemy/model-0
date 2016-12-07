883f654173686a1e712225f056adc6d3462453dc
jessezhang91 <jessezhang91@gmail.com>
2016-12-06 22:59:21 -0500

    move fromJSON to transformable

--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,3 @@
 export abstract class Model {
-	static fromJSON(_: Object): Model {
-		throw new Error("Unimplemented");
-	}
+
 }
--- src/transformation/transformable.ts
+++ src/transformation/transformable.ts
@@ -3,6 +3,10 @@ import { Json } from "./json";
 import { toJSON } from "./utilities";
 
 export abstract class Transformable<M extends Transformable<M>> extends Model {
+	static fromJSON<M extends Transformable<M>>(_: Json<M>): M {
+		throw new Error("Unimplemented");
+	}
+
 	toJSON(this: M): Json<M> {
 		return toJSON(this);
 	}
